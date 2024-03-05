0cba12de6b35fa5c2d3b66048d0b4062aa68973a
crime <francolinoflavio@gmail.com>
2024-03-05 22:43:42 +0100

    refactor

--- src/main.py
+++ src/main.py
@@ -11,11 +11,9 @@ import time
 
 enable_tf_gpu_growth()
 
-# cifar10_model_conf = mf.ModelConf('cifar10', cifar100, 10, (4, 4), (32, 32, 3))
-
-
 
 print("starting cifar10 dataset")
+
 # cifar10
 cifar10_model_conf = mf.ModelConf('cifar10', cifar10, 10, (4, 4), (32, 32, 3))
 server_cifar10 = Server.Server(cifar10_model_conf)
@@ -28,20 +26,20 @@ time.sleep(10)
 print("starting mnist dataset")
 
 # # mnist
-# mnist_model_conf = mf.ModelConf('mnist', mnist, 10, (3, 3), (28, 28, 1))
-# server_mnist = Server.Server(mnist_model_conf)
-# server_mnist.start_simulation()
-# del server_mnist
-# del mnist_model_conf
-# print("finish mnist")
-#
-# time.sleep(10)
-# print("starting cifar100 dataset")
-#
-# # cifar100
-# cifar100_model_conf = mf.ModelConf('cifar100', cifar100, 100, (4, 4), (32, 32, 3))
-# server_cifar100 = Server.Server(cifar100_model_conf)
-# server_cifar100.start_simulation()
-# del server_cifar100
-# del cifar100_model_conf
-# print("finish cifar100")
+mnist_model_conf = mf.ModelConf('mnist', mnist, 10, (3, 3), (28, 28, 1))
+server_mnist = Server.Server(mnist_model_conf)
+server_mnist.start_simulation()
+del server_mnist
+del mnist_model_conf
+print("finish mnist")
+
+time.sleep(10)
+print("starting cifar100 dataset")
+
+# cifar100
+cifar100_model_conf = mf.ModelConf('cifar100', cifar100, 100, (4, 4), (32, 32, 3))
+server_cifar100 = Server.Server(cifar100_model_conf)
+server_cifar100.start_simulation()
+del server_cifar100
+del cifar100_model_conf
+print("finish cifar100")
